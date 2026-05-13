abb293d6e2368e3a46cae5e2341eb232b618def2
shendiA6 <hanzstok224@gmail.com>
2026-05-13 19:20:12 +0700

    Update manajemen gudang

---
+++ ManajemenGudang.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+struct Barang{
+    int id;
+    string nama;       //struct
+    int stok;
+};
+
+struct Node{
+    Barang data;    //pointer
+    Node* next;
+};
+
+
+int main(){
+
+
+    return 0;
+}
